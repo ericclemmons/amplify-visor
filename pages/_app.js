ec6d47bda68e258fb8f0c679b5cb1037829a5bd9
Eric Clemmons <eric@smarterspam.com>
2021-04-27 13:40:46 -0500

    Add Tailwind UI
    https://tailwindui.com/components/application-ui/page-examples/settings-screens#component-5357b57a5c14addf36e64050366465d4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-gray-100">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: colors.orange,
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
