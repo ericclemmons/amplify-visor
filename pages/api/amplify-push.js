f60839a7ee3a49e2580b3e334e8040ddbe7d0463
Eric Clemmons <eric@smarterspam.com>
2021-04-29 11:15:15 -0500

    Fix /api/amplify-push

--- pages/api/amplify-push.js
+++ pages/api/amplify-push.js
@@ -1,63 +1,32 @@
-// import { nspawn as spawn } from "../../utils/nexpect";
+import { nspawn } from "../../utils/nexpect";
 import Convert from "ansi-to-html";
-import { spawn } from "child_process";
 
-export default async function addAuth(req, res) {
-  let body;
-  try {
-    body = JSON.parse(req.body);
-  } catch (e) {
-    res.send(400).json({ e });
-  }
+const convert = new Convert({
+  fg: "#eee",
+  bg: "#222",
+  newline: false,
+  escapeXML: true,
+  stream: true,
+});
 
-  const { cwd } = body;
+const pushTimeoutMS = 1000 * 60 * 20; // 20 minutes;
 
-  res.write("Pushing your changes to the cloud...\n");
-  res.write("This may take a few minutes...\n");
-
-  //   spawn("amplify", ["push", "-y"], {
-  //     cwd,
-  //     stripColors: true,
-  //     stdio: "inherit",
-  //   })
-  //     .wait("All resources are updated in the cloud")
-  //     .sendEof()
-  //     .run((err) => {
-  //       if (!err) {
-  //         console.log("success");
-  //         res.write("Succesfuly pushed changes to the cloud!");
-  //       } else {
-  //         res.write("Error!", err);
-  //         res.write("error", err);
-  //       }
-  //       res.end();
-  //     });
-  res.write("amplify push -y\n");
-  const pushChanges = spawn("amplify", ["push", "-y"], {
-    cwd,
+// https://github.com/aws-amplify/amplify-cli/blob/master/packages/amplify-e2e-core/src/init/amplifyPush.ts
+export default async function amplifyPush(req, res) {
+  const chain = nspawn("amplify", ["push", "--force"], {
+    noOutputTimeout: pushTimeoutMS,
   });
 
-  pushChanges.stdout.on("data", (data) => {
-    const convert = new Convert({
-      fg: "#eee",
-      bg: "#222",
-      newline: false,
-      escapeXML: true,
-      stream: true,
+  chain
+    .wait("Are you sure you want to continue?")
+    .sendLine("y")
+    .wait(/.*/)
+    .run((err) => {
+      res.status(err ? 500 : 200);
+      res.end();
     });
 
-    res.write(convert.toHtml(String(data)));
-  });
-
-  pushChanges.on("close", (code) => {
-    console.log(`push amplify process closed with code ${code}`);
-
-    if (code !== 0) {
-      res.end();
-      return;
-    }
-
-    res.write("🚀 Push Completed!");
-    res.end();
-  });
+  chain.context.process.addOnDataHandler((content) =>
+    res.write(convert.toHtml(content))
+  );
 }
